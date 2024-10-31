199812f35c6042fb9dc469c4cfcaf40f4c41aa6e
fitrifityanto <fitrifityanto@gmail.com>
2024-11-01 06:09:50 +0700

    add: menu mobile dengan icon bar, favicon, title & desc meta tags

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
 const currentYear = new Date().getFullYear()
 
 document.getElementById('currentYear').textContent = currentYear;
+
+const menuMobile = document.getElementById('menuMobile');
+const hiddenBox = document.getElementById('hiddenBox');
+
+const iconBar = document.getElementById('mobileIcon').firstElementChild;
+const iconClose = document.getElementById('mobileIcon').lastElementChild;
+
+
+iconBar.addEventListener('click', () => {
+  menuMobile.style.display = 'block';
+  iconBar.style.display = 'none';
+  iconClose.style.display = 'block';
+  hiddenBox.style.display = 'block';
+});
+
+iconClose.addEventListener('click', () => {
+  menuMobile.style.display = 'none';
+  iconBar.style.display = 'block';
+  iconClose.style.display = 'none';
+  hiddenBox.style.display = 'none';
+});
